760152b68b31e98a2c5e57ec21724bfda34b27d4
Javier Cazallas <jcazallasc@gmail.com>
2020-05-28 11:05:21 +0200

    Update views

--- resources/views/discussions/show.blade.php
+++ resources/views/discussions/show.blade.php
@@ -10,4 +10,51 @@
         {!! $discussion->content !!}
     </div>
 </div>
+
+@foreach ($discussion->replies()->paginate(env('ITEMS_PER_PAGE')) as $index => $reply)
+    <div class="card my-5">
+        <div class="card-header">
+            <div class="d-flex justify-content-between">
+                <div>
+                    <img src="{{ Gravatar::src($reply->owner->email) }}" alt="{{ $reply->owner->name }}" class="rounded-circle w-25 mr-2"/>
+                    <span>{{ $reply->owner->name }}</span>
+                </div>
+            </div>
+        </div>
+        <div class="card-body">
+            {!! $reply->content !!}
+        </div>
+    </div>
+@endforeach
+{{ $discussion->replies()->paginate(env('ITEMS_PER_PAGE'))->links() }}
+
+<div class="card my-5">
+    <div class="card-header">Add a reply</div>
+
+    <div class="card-body">
+        @auth
+            @include('partials.errors')
+            <form action="{{ route('replies.store', $discussion->slug) }}" method="POST">
+                @csrf 
+                <div class="form-group">
+                    <input type="hidden" id="content" name="content" value="{{ old('content') }}">
+                    <trix-editor input="content"></trix-editor>
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-sm btn-success">Add reply</button>
+                </div>
+            </form> 
+        @else
+            <a href="{{ route('login') }}" class="btn btn-primary btn-block">Sign in to add a reply</a>
+        @endauth
+    </div>
+</div>
+@endsection
+
+@section('scripts')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.2.1/trix.js"></script>
+@endsection
+
+@section('css')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.2.1/trix.css">
 @endsection
--- resources/views/partials/discussions/header.blade.php
+++ resources/views/partials/discussions/header.blade.php
@@ -1,7 +1,7 @@
 <div class="card-header">
     <div class="d-flex justify-content-between">
         <div>
-            <img src="{{ Gravatar::src($discussion->author->email) }}" alt="{{ $discussion->author->name }}" class="mr-2" width="40px" height="40px" style="border-radius: 50%">
+            <img src="{{ Gravatar::src($discussion->author->email) }}" alt="{{ $discussion->author->name }}" class="mr-2 rounded-circle w-25"/>
             <h5 class="d-inline font-weight-bold">{{ $discussion->author->name }}</h5>
         </div>
         <div>
